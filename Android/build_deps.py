8e9a67be988bca53c8fa0be2b14d622340af1bb0
Flood Ryan A <A0012350@kiongroup.com>
2023-10-23 13:49:58 -0500

    DWE-3790: Update OpenSSL source

--- Android/build_deps.py
+++ Android/build_deps.py
@@ -85,7 +85,7 @@ class NCurses(Package):
     configure_args = ['--without-ada', '--enable-widec', '--without-debug', '--without-cxx-binding', '--disable-stripping']
 
 class OpenSSL(Package):
-    source = 'https://www.openssl.org/source/openssl-3.1.3.tar.gz'
+    source = 'https://www.openssl.org/source/openssl-3.0.11.tar.gz'
 
     def configure(self):
         # OpenSSL handles NDK internal paths by itself
